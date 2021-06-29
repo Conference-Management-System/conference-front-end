68a594b31cdf0ae7a8a6d665883f90eb956224f9
Kajathees Premendran <60744394+kajan03@users.noreply.github.com>
2021-06-29 11:48:10 +0530

    user register page update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Reactdom from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./src/pages/home";
+import Register from "./src/pages/userRegister";
 
 function App(props) {
   return (
@@ -9,7 +10,8 @@ function App(props) {
       <Router>
         <section>
           <Switch>
-            <Route path="/" component={Home} />
+            <Route path="/" component={Home} exact />
+            <Route path="/register" component={Register} />
           </Switch>
         </section>
       </Router>
---
+++ src/pages/userRegister.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { Grid, Container, Card, CardContent } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import axiosInstance from "../services/axiosInstance";
+
+export default function userRegister() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axiosInstance.post("/register", {
+        username,
+        email,
+        password,
+        type,
+      });
+      console.log(res.data) && alert("registered successfully");
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Grid container justify="center">
+          <Grid item sm={4}></Grid>
+          <Grid style={{ paddingTop: "100px" }} item sm={4}>
+            <Card>
+              <CardContent>
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="username" className="form-label">
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </div>
+                  {/* <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">
+                      User type
+                    </label>
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                      value={userType}
+                    >
+                      <option selected>Select Type</option>
+                      <option value="researcher">Researcher</option>
+                      <option value="workshop Conductor">
+                        workshop Conductor
+                      </option>
+                      <option value="Attendee">Attendee</option>
+                    </select>
+                  </div> */}
+                  <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">
+                      User Type
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => {
+                        setType(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">
+                    Register
+                  </button>
+                </form>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item sm={4}></Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
