f7d50cb330ccbb2bb421f8c74b2bbeda0b871f2e
Sidpavn <sidsterlk@gmail.com>
2021-06-29 22:47:10 +0530

    Added Workshop and Reviewer Part

---
+++ src/components/workshopTableRow.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+export default class WorkshopTableRow extends Component {
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.obj.title}</td>
+                <td>{this.props.obj.conducter_name}</td>
+                <td>{this.props.obj.mobile}</td>          
+                <td>{this.props.obj.createdAt}</td>  
+                <td>{this.props.obj.document}</td>  
+                <td>{this.props.obj.status}</td>  
+                <td>
+                    <Link className="update-link" to={"/workshop-review/" + this.props.obj._id}>
+                        <button type="button" class="btn btn-primary">
+                              Review Submission 
+                        </button>
+                    </Link>                    
+                </td>      
+            </tr>
+        );
+    }
+}
