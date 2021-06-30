9aae138642697f992f7d4fa38dd9f1f4304d8a52
irfan <sm2irfan@gmail.com>
2021-06-30 23:15:15 +0530

    APP
    App impli

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Reactdom from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./src/pages/home";
+import Register from "./src/pages/userRegister";
+import SubmitResearch from "./src/pages/submitResearch";
+import SubmitWorkshop from "./src/pages/submitWorkshop";
+import Login from "./src/pages/userLogin";
+import Research from "./src/pages/research";
+import Workshop from "./src/pages/workshop";
+import { useContext } from "react";
+import { Context } from "./src/context/context";
+import ResearchApproval from "./src/components/reviewer/researchApproval";
+import ResearchReview from "./src/components/reviewer/researchReview";
+import WorkshopReview from "./src/components/reviewer/workshopReview";
+import ReviewerDashboard from "./src/components/reviewer/reviewerDashboard";
+import WorkshopApproval from "./src/components/reviewer/workshopApproval";
+import createConferenceDetails from "./src/components/editer/createConferenceDetails";
+import Allconference from "./src/components/editer/conferenceDetails";
+import editConference from "./src/components/editer/ConferenceDetailsEdit";
+function App() {
+  const { user } = useContext(Context);
+  return (
+    <div>
+      <Router>
+        <section>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/register" component={Register} />
+            <Route path="/create-research" component={SubmitResearch} />
+            <Route path="/create-workshop" component={SubmitWorkshop} />
+            {/* <Route exact path="/login">
+              {user ? <Home /> : <Login />}
+            </Route> */}
+            <Route path="/workshop" component={Workshop} />
+            <Route path="/research" component={Research} />
+            <Route path="/research-paper" component={ResearchApproval} />
+            <Route path="/research-review/:id" component={ResearchReview} />
+            <Route path="/workshop-review/:id" component={WorkshopReview} />
+            <Route path="/workshop-list" component={WorkshopApproval} />
+            <Route path="/reviewer" component={ReviewerDashboard} exact />
+            <Route
+              path="/create-conference"
+              component={createConferenceDetails}
+            />
+            <Route path="/Allconference" component={Allconference} />
+            <Route path="/editConference/:id" component={editConference} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Router>
+    </div>
+  );
+}
+export default App;
--- index.js
+++ index.js
@@ -1,40 +1,10 @@
 import React from "react";
-import Reactdom from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./src/pages/home";
-
-import createConferenceDetails from './src/components/editer/createConferenceDetails';
-import Allconference from './src/components/editer/conferenceDetails';
-import editConference from './src/components/editer/ConferenceDetailsEdit';
-import admonApprove from './src/components/editer/adminApprove';
-
-import Register from "./src/pages/userRegister";
-import Research from "./src/pages/submitResearch";
-import Workshop from "./src/pages/submitWorkshop";
-
-function App(props) {
-  return (
-    <div>
-      <Router>
-        <section>
-          <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path = "/create-conference" component = {createConferenceDetails}/>
-            <Route path = "/Allconference" component = {Allconference}/>
-            <Route path = "/editConference/:id" component = {editConference}/>
-
-            <Route path = "/admonApprove" component = {admonApprove}/>
-
-
-            <Route path="/register" component={Register} />
-            <Route path="/research" component={Research} />
-            <Route path="/workshop" component={Workshop} />
-
-          </Switch>
-        </section>
-      </Router>
-    </div>
-  );
-}
-
-Reactdom.render(<App />, document.getElementById("root"));
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ContextProvider } from "./src/context/context";
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
